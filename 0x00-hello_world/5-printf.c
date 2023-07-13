1bc6342342e52cac811b594e422d7de2637cde3f
Obada-Badee <obadabadee3@hotmail.com>
2023-07-13 10:51:30 -0700

    4TH script

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- *main - Entry point
- *Return: Always 0 (Success)
+ * main - starting point
+ * Return: always 0 (Succes)
  */
+
 int main(void)
 {
-      printf("with proper grammar, but the outcome is a piece of art,\n");
-      return (0);
+  printf("with proper grammar, but the outcome is a piece of art,\n");
+  return (0):
 }
