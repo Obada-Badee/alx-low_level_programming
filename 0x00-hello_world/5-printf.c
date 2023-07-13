a016aae75aaaeb21d7bc828375d607fb0d8182a8
Obada b33 <obadabadee3@hotmail.com>
2023-07-13 18:13:53 +0300

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
-*main - entry point
-*return with 0
-*/
+ *main - entry point
+ *return with 0
+ */
 
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
-
 return (0);
 }
