ea8448ffb3db3db0e578b3d094e02e557eacea6f
Obada b33 <obadabadee3@hotmail.com>
2023-07-19 00:48:30 +0300

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,18 @@
 #include "main.h"
 
 /**
- * print_alphabet - prints the alphabet in liower cases
- * Return: 0
+ *print_alphabet - prints the alphabet in liower cases
+ *Return: 0
  */
 
 void print_alphabet(void)
 {
-   char start;
-   
-   for (start = 'a'; start <= 'z'; start++)
-   {
-         _putchar(start);
-   }
-   _putchar('\n');
+	char start;
+
+	for (start = 'a'; start <= 'z'; start++)
+	{
+		_putchar(start);
+	}
+
+	_putchar('\n');
 }
