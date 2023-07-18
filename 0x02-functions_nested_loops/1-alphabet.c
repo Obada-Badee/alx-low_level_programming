8429c49b0c616eb943e6ac7c6f5115efc7595a18
Obada b33 <obadabadee3@hotmail.com>
2023-07-19 00:25:42 +0300

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,15 +2,16 @@
 
 /**
  * print_alphabet - prints the alphabet in liower cases
- * Return : 0
+ * Return: 0
  */
 
 void print_alphabet(void)
 {
-  char start;
-
-  for (start = 'a'; start <= 'z'; start++)
-    _putchar(start);
-
-  _putchar("\n");
+        char start;
+        
+          for (start = 'a'; start <= 'z'; start++)
+          {
+                  _putchar(start);
+          }
+        _putchar("\n");
 }
