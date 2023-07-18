2bd4fdf4086c45dd2870499e2147566c4186a0fb
Obada-Badee <obadabadee3@hotmail.com>
2023-07-18 14:21:10 -0700

    Solution done

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet in liower cases
+ * Return : 0
+ */
+
+void print_alphabet(void)
+{
+  char start;
+
+  for (start = 'a'; start <= 'z'; start++)
+    _putchar(start);
+
+  _putchar("\n");
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,12 @@
+
+/**
+ *_putchar - print the character
+ */
+
 int _putchar(char c);
 
 /**
  * print_alphabet - print lowercase alphabet
  */
+
+void print_alphabet(void)
