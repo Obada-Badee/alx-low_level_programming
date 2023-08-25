cc4966b37beb6e4aabc161bfa20dd4de39154587
Obada A.Albadee <obadabadee3@hotmail.com>
2023-08-25 05:35:23 +0300

    BETTY FIX 00

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,26 +1,25 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "lists.h"
 
 /**
- * print_list - check the code for Holberton School students.
- * @h: name of the list
- * Return: the number of nodes.
+ * print_list - Print the data stored in the nodes of a linked list
+ * @h: A pointer to the first node in the linked list
+ *
+ * Return: The number of nodes
  */
 size_t print_list(const list_t *h)
 {
-    int count = 0;
+	size_t num_nodes = 0;
 
-    while (h)
-    {
-        if (h->str == NULL)
-            printf("[0] (nil)\n");
-        else
-            printf("[%d] %s\n", h->len, h->str);
+	while (h)
+	{
+		if (!(h->str))
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
 
-        count++;
-        h = h->next;
-    }
-    return (count);
+		num_nodes++;
+		h = h->next;
+	}
+
+	return (num_nodes);
 }
