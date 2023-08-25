138d1a14ee12e396cdbf12058ef3b21cb5c0249a
Obada A.Albadee <obadabadee3@hotmail.com>
2023-08-25 05:32:46 +0300

    BETTY FIX 00

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,14 +10,15 @@
  */
 size_t print_list(const list_t *h)
 {
-	int count = 0;
-
-	while (h)
+    int count = 0;
+    
+    while (h)
 	{
-    if (h->str == NULL)
-		printf("[0] (nil)\n");
-	else
-		printf("[%d] %s\n", h->len, h->str);
+        if (h->str == NULL)
+		    printf("[0] (nil)\n");
+	    else
+		    printf("[%d] %s\n", h->len, h->str);
+        
 		count++;
 		h = h->next;
 	}
