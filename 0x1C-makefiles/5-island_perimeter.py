f4be99160cf35396b9a55bbf2586010f6c978c88
Obada-Badee <obadabadee3@hotmail.com>
2024-02-29 18:58:48 +0300

    Solve all

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""A module that Calculates the perimeter of an island"""
+
+
+def island_perimeter(grid):
+    """Calculate the perimeter of the island described in grid"""
+
+    row = len(grid)
+    col = len(grid[0])
+    perimeter = 0
+
+    for i in range(1, row):
+        for j in range(1, col):
+            if (grid[i][j] == 1):
+                perimeter += 4
+                if (grid[i - 1][j] == 1):
+                    perimeter -= 1
+                if (grid[i + 1][j] == 1):
+                    perimeter -= 1
+                if (grid[i][j - 1] == 1):
+                    perimeter -= 1
+                if (grid[i][j + 1] == 1):
+                    perimeter -= 1
+    return perimeter
+                
